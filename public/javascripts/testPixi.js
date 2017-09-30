b254c88ee1fa825d96a9e0d2036825a9d25507e7
mstop4 <mstop4@gmail.com>
2017-09-30 12:24:20 -0400

    full window canvas with border

--- public/javascripts/testPixi.js
+++ public/javascripts/testPixi.js
@@ -4,3 +4,23 @@ if(!PIXI.utils.isWebGLSupported()){
 }
 
 PIXI.utils.sayHello(type)
+
+//Create the renderer
+let renderer = PIXI.autoDetectRenderer(256, 256)
+renderer.view.style.border = "1px dashed black"
+renderer.backgroundColor = 0x808080
+
+// Full window canvas
+renderer.view.style.position = "absolute";
+renderer.view.style.display = "block";
+renderer.autoResize = true;
+renderer.resize(window.innerWidth, window.innerHeight);
+
+//Add the canvas to the HTML document
+document.body.appendChild(renderer.view)
+
+//Create a container object called the `stage`
+let stage = new PIXI.Container()
+
+//Tell the `renderer` to `render` the `stage`
+renderer.render(stage)
