198a1d977ef2f5ff7fc24d39c0d226a3a8690e65
mstop4 <mstop4@gmail.com>
2017-09-30 12:43:03 -0400

    bunny in browser

--- public/javascripts/testPixi.js
+++ public/javascripts/testPixi.js
@@ -5,16 +5,28 @@ if(!PIXI.utils.isWebGLSupported()){
 
 PIXI.utils.sayHello(type)
 
+// Sprite Loader setup
+function setup() {
+  var sprBunny = new PIXI.Sprite(
+    PIXI.loader.resources["../images/bunny.jpg"].texture
+  )
+
+  stage.addChild(sprBunny)
+  
+  //Tell the `renderer` to `render` the `stage`
+  renderer.render(stage)
+}
+
 //Create the renderer
 let renderer = PIXI.autoDetectRenderer(256, 256)
 renderer.view.style.border = "1px dashed black"
 renderer.backgroundColor = 0x808080
 
 // Full window canvas
-renderer.view.style.position = "absolute";
-renderer.view.style.display = "block";
-renderer.autoResize = true;
-renderer.resize(window.innerWidth, window.innerHeight);
+renderer.view.style.position = "absolute"
+renderer.view.style.display = "block"
+renderer.autoResize = true
+renderer.resize(window.innerWidth, window.innerHeight)
 
 //Add the canvas to the HTML document
 document.body.appendChild(renderer.view)
@@ -22,5 +34,7 @@ document.body.appendChild(renderer.view)
 //Create a container object called the `stage`
 let stage = new PIXI.Container()
 
-//Tell the `renderer` to `render` the `stage`
-renderer.render(stage)
+// Load sprites to cache
+PIXI.loader
+  .add("../images/bunny.jpg")
+  .load(setup)
