cb01f75f3f250f02251cedf29ce300ed7a81f251
mstop4 <mstop4@gmail.com>
2017-10-01 12:25:58 -0400

    trying to divide video:

--- public/javascripts/video.js
+++ public/javascripts/video.js
@@ -1,7 +1,14 @@
 let renderer = undefined
 let stage = undefined
-let videos = []
-let textureURIs = ["../videos/escalator.mp4"]
+let sprites = []
+let textureURIs = ["../videos/bunny.mp4"]
+let videoContainer = undefined
+
+let videoScale = 1
+let numRows = 2
+let numColumns = 2
+let xOffset = 0
+let yOffset = 0
 
 let frameSkip = 0
 let fsIndex = 0
@@ -12,18 +19,41 @@ loadTextures(textureURIs, setup)
 function setup() {
     console.log("Setting up video...")
 
-    let texture = PIXI.Texture.fromVideo(PIXI.loader.resources[textureURIs[0]].data);
-    
-    // create a new Sprite using the video texture (yes it's that easy)
-    let video = new PIXI.Sprite(texture);
+    videoContainer = new PIXI.Container()
+    stage.addChild(videoContainer)
+
+    let cellWidth = 1280 / numColumns
+    let cellHeight = 720 / numRows
+
+    for (let i = 0; i < numRows; i++) {
+        for (let j = 0; j < numColumns; j++) {
+
+            let rect = new PIXI.Rectangle(j*cellWidth, i*cellHeight, cellWidth, cellHeight)
+            let texture = PIXI.Texture.fromVideo(PIXI.loader.resources[textureURIs[0]].data)
+            texture.frame = rect
+            texture.baseTexture.source.loop = true;
+            let newSprite = new PIXI.Sprite(texture)
+
+            newSprite.x = xOffset + j*(cellWidth)
+            newSprite.y = yOffset + i*(cellHeight)
+            newSprite.vx = 0
+            newSprite.vy = 0
+            newSprite.width = cellWidth
+            newSprite.height = cellHeight
+            //newSprite.anchor.set(0.5, 0.5)
+            newSprite.physicsOn = false
+            sprites.push(newSprite)
+
+            console.log(j*cellWidth, i*cellHeight, cellWidth, cellHeight, newSprite.x, newSprite.y, newSprite.width, newSprite.height)
+
+            videoContainer.addChild(newSprite)
+        }
+    }
 
     // Stetch the fullscreen
-    video.width = 640;
-    video.height = 360;
-    video.texture.baseTexture.source.loop = true;
-    console.dir(video.texture.baseTexture.source.loop )
-
-    stage.addChild(video); 
+    // video.scale.x = videoScale
+    // video.scale.y = videoScale
+    // video.texture.baseTexture.source.loop = true;
 
     gameLoop()
   }
