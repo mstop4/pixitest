e949ffcfce653f16aff4d1284fb97f0faaf155a4
mstop4 <mstop4@gmail.com>
2017-09-30 21:14:23 -0400

    make exploding bunny the index for demo reasons

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/bounce', index);
 app.use('/video', video);
-app.use('/explode/', explode);
+app.use('/', explode);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
